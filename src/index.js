d6c03349bfbc54356088e83eaae3b528828aaa46
yasira <yasirayusif@gmail.com>
2022-06-20 12:44:18 -0700

    fifth

--- src/index.js
+++ src/index.js
@@ -4,6 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "tailwindcss/base";
+import "./custom-base-styles.css";
+
+import "tailwindcss/components";
+import "./custom-components.css";
+
+import "tailwindcss/utilities";
+import "./custom-utilities.css";
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [ "./**/*.{html,js}"],
+  content: ["./**/*.{html,js}"],
+  // "./src/**/*.{js,jsx,ts,tsx}"
   theme: {
     extend: {},
   },
