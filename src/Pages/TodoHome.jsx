10cf14be6c28f0a5dc70a04fe6fc064857f6ce5d
yasira <yasirayusif@gmail.com>
2022-06-20 17:24:53 -0700

    21

--- src/Pages/TodoHome.jsx
+++ src/Pages/TodoHome.jsx
@@ -4,10 +4,30 @@ import TodoList from '../Components/TodoList'
 
 const TodoHome = () => {
   return (
-    <div>
-          <TodoForm />
+          
+          <div className="my-4 mx-auto md:mx-auto text-black text-center">
+            
+            <h2 className=" text-2xl font-extrabold p-2">The Task Planner</h2>
+            
+                <TodoForm /> 
+
+            {/* { (todos.length > 0 ) ?
+            
+                ( (todoLoaderStarted) ?
+                    
+                    (<div>todo loading please wait...</div>) :
+
+                    (todos.map((todo, index) => (<TodoList task={todo} key={index} />)))                   
+                
+                )
+
+                : (<div>No To_dos Added Yet </div>)
+                
+            }  */}
+
           <TodoList/>
-    </div>
+           
+        </div>
   )
 }
 
